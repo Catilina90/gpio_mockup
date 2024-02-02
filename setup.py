dde41d8dc03483ff2642a9bce831e1a0039d068d
Catilina90 <107310345+Catilina90@users.noreply.github.com>
2024-02-02 22:56:26 +0100

    Update setup.py RPI --> RPi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = "RPI_GPIO",
+    name = "RPi",
     version = "1.0.0",
     author='Yvonne',
     description='Package to provide dead end-calls for package RPI_GPIO (e.g. usefull when testing in Pipelines)',
-    packages=['RPI_GPIO']
+    packages=['RPi']
 )

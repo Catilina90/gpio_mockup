25b1f6f05bef1527222631482a1b403b6e7909d2
Catilina90 <yvonne.behres@gmail.com>
2024-03-02 14:46:48 +0100

    fix package structure

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version = "1.0.0",
     author='Yvonne',
     description='Package to provide dead end-calls for package RPI_GPIO (e.g. usefull when testing in Pipelines)',
-    packages=[]
+    packages=['RPi']
 )

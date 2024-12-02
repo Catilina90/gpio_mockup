41c1dea507a5393fd2435a2836340e769c29ad9f
Catilina90 <yvonne.behres@gmail.com>
2024-12-02 22:20:52 +0100

    transform to pyproject.toml package structure

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name = "RPi",
-    version = "1.0.0",
-    author='Yvonne',
-    description='Package to provide dead end-calls for package RPI_GPIO (e.g. usefull when testing in Pipelines)',
-    packages=['RPi']
-)

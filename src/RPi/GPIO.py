4ac67450f386bda6373ce5b83e3826bf9ac328c2
Catilina90 <yvonne.behres@gmail.com>
2025-02-08 17:48:38 +0100

    reduced debugging output

--- src/RPi/GPIO.py
+++ src/RPi/GPIO.py
@@ -17,7 +17,6 @@ def setmode(*args, **kwargs):
     # initiate with creating a json file for tracking states
     with resources.path("RPi.data", "board_config.json") as path:
         with open(path, "w") as board_config:
-            print(f"Config Path: {board_config}")
             config = []
             json.dump(config, board_config)
 
@@ -26,13 +25,12 @@ def setup(channel, setting, state=LOW):
     with resources.path("RPi.data", "board_config.json") as path:
         with open(path, "r") as board_config:
             config = json.load(board_config)
-            print(f"before append: {config}")
             # TODO: Check if channel is already configured
 
     with resources.path("RPi.data", "board_config.json") as path:
         with open(path, "w") as board_config:
             config.append({"channel": channel, "setting": setting, "state": state})
-            print(f"after append: {config}")
+            print(f"New config: {config}")
             json.dump(config, board_config)
 
 
@@ -42,7 +40,6 @@ def output(channel, state):
             config = json.load(board_config)
 
             _ = [i.update({"state": state}) for i in config if i["channel"] == channel]
-            print(f"output config: {config}")
 
     with resources.path("RPi.data", "board_config.json") as path:
         with open(path, "w") as board_config:
